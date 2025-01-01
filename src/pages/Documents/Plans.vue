fecd980c38721a952a116a835e66fc0e05dd7529
SarVar <sarvarrozikulov9@gmail.com>
2025-01-02 02:11:47 +0500

    update docs

---
+++ src/pages/Documents/Plans.vue
@@ -0,0 +1,63 @@
+<script>
+import Sections from "@/components/Sections.vue";
+import DocumentItem from "@/pages/Documents/DocumentItem.vue";
+import {mapGetters} from "vuex";
+import api from "@/axios";
+
+export default {
+  name: "Plans",
+  components: {DocumentItem, Sections},
+  props:{
+    title: {
+      type: String,
+      default: "{ plans }"
+    },
+  },
+  data(){
+    return{
+      content: [],
+    }
+  },
+  computed: {
+    ...mapGetters(['currentLanguage']),
+  },
+  watch: {
+    currentLanguage(newLang) {
+      this.getImportant();
+    },
+  },
+  mounted() {
+    this.getImportant();
+  },
+  methods:{
+    getImportant() {
+      api.get(`/plans-for2025/?lang_code=${this.currentLanguage}`)
+          .then((response) => {
+            if (Array.isArray(response.data)) {
+              this.content = response.data;
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    },
+  },
+}
+</script>
+
+<template>
+  <div>
+    <sections>
+      <template #title>{{title}}</template>
+      <template #content>
+        <div style="display: flex; flex-direction: column; gap: 1rem">
+          <document-item v-for="c in content" :key="c.id" :title="c.title" :docs="c.file"/>
+        </div>
+      </template>
+    </sections>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
