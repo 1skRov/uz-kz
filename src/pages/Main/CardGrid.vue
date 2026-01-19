4de749ffcb5ff136705a7f5151162c38c7957ff3
sarvar_rozikulov <sarvarrozikulov9@gmail.com>
2026-01-19 19:12:20 +0500

    update

--- src/pages/Main/CardGrid.vue
+++ src/pages/Main/CardGrid.vue
@@ -1,62 +1,62 @@
 <script>
-import {BASE_URL} from "@/axios";
-import {Swiper, SwiperSlide} from "swiper/vue";
+  import { BASE_URL } from '@/axios';
+  import { Swiper, SwiperSlide } from 'swiper/vue';
 
-export default {
-  name: "CardGrid",
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
-  props: {
-    cards: {
-      type: Array,
-      required: true,
+  export default {
+    name: 'CardGrid',
+    components: {
+      Swiper,
+      SwiperSlide,
     },
-  },
-  data() {
-    return {
-      BASE_URL,
-      isMobile: false,
-    };
-  },
-  mounted() {
-    this.isMobile = window.innerWidth < 540;
-    window.addEventListener("resize", this.handleResize);
-  },
-  beforeDestroy() {
-    window.removeEventListener("resize", this.handleResize);
-  },
-  methods: {
-    handleResize() {
+    props: {
+      cards: {
+        type: Array,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        BASE_URL,
+        isMobile: false,
+      };
+    },
+    mounted() {
       this.isMobile = window.innerWidth < 540;
+      window.addEventListener('resize', this.handleResize);
     },
-    openDetails(id) {
-      this.$router.push({
-        name: "FamousPersonDetails",
-        params: {id},
-      });
+    beforeDestroy() {
+      window.removeEventListener('resize', this.handleResize);
     },
-  },
-};
+    methods: {
+      handleResize() {
+        this.isMobile = window.innerWidth < 540;
+      },
+      openDetails(id) {
+        this.$router.push({
+          name: 'FamousPersonDetails',
+          params: { id },
+        });
+      },
+    },
+  };
 </script>
 
 <template>
   <div class="hero-section">
     <Swiper
-        v-if="isMobile"
-        :slides-per-view="1.5"
-        :space-between="30"
-        class="mobile-swiper"
+      v-if="isMobile"
+      :slides-per-view="1.5"
+      :space-between="30"
+      class="mobile-swiper"
     >
       <SwiperSlide
-          v-for="(card, index) in cards"
-          :key="card.id"
-          @click="openDetails(card.id)"
+        v-for="(card, index) in cards"
+        :key="card.id"
+        @click="openDetails(card.id)"
       >
         <div
-            class="swiper-card__background"
-            :style="{ backgroundImage: `url(${BASE_URL + card.image})` }"
+          class="swiper-card__background"
+          :style="{ backgroundImage: `url(${BASE_URL + card.image})` }"
         >
           <div class="swiper-card__overlay">
             <div class="swiper-card__content">
@@ -70,14 +70,14 @@ export default {
 
     <div class="card-grid" v-else>
       <div
-          class="card"
-          v-for="(card, index) in cards"
-          :key="card.id"
-          @click="openDetails(card.id)"
+        class="card"
+        v-for="(card, index) in cards"
+        :key="card.id"
+        @click="openDetails(card.id)"
       >
         <div
-            class="card__background"
-            :style="{ backgroundImage: `url(${BASE_URL + card.image})` }"
+          class="card__background"
+          :style="{ backgroundImage: `url(${BASE_URL + card.image})` }"
         ></div>
         <div class="card__overlay">
           <div class="card__content">
@@ -91,148 +91,148 @@ export default {
 </template>
 
 <style scoped>
-.hero-section {
-  display: flex;
-  justify-content: center;
-}
+  .hero-section {
+    display: flex;
+    justify-content: center;
+  }
 
-.card-grid {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 20px;
-  width: 100%;
-}
-
-@media (min-width: 540px) and (max-width: 959px) {
   .card-grid {
-    grid-template-columns: repeat(4, 1fr);
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+    width: 100%;
   }
-}
 
-@media (min-width: 960px) {
-  .card-grid {
-    grid-template-columns: repeat(4, 1fr);
+  @media (min-width: 540px) and (max-width: 959px) {
+    .card-grid {
+      grid-template-columns: repeat(4, 1fr);
+    }
   }
-}
 
-.card {
-  position: relative;
-  overflow: hidden;
-  border-radius: 8px;
-  transition: transform 0.3s ease, box-shadow 0.3s ease;
-}
-
-.card:hover {
-  transform: scale(1.015);
-  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.2);
-}
-
-.card__background {
-  background-size: cover;
-  background-position: center;
-  width: 100%;
-  height: 0;
-  padding-bottom: 150%;
-}
-
-.card__overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 114, 171, 0.9);
-  opacity: 0;
-  visibility: hidden;
-  transition: opacity 0.3s ease, visibility 0.3s ease;
-  display: flex;
-  align-items: flex-end;
-  border-radius: 8px;
-  padding: 20px;
-  box-sizing: border-box;
-}
-
-@media (min-width: 960px) {
-  .card:hover .card__overlay {
-    opacity: 1;
-    visibility: visible;
+  @media (min-width: 960px) {
+    .card-grid {
+      grid-template-columns: repeat(4, 1fr);
+    }
   }
-}
 
-@media (max-width: 959px) {
+  .card {
+    position: relative;
+    overflow: hidden;
+    border-radius: 8px;
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+  }
+
+  .card:hover {
+    transform: scale(1.015);
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.2);
+  }
+
+  .card__background {
+    background-size: cover;
+    background-position: center;
+    width: 100%;
+    height: 0;
+    padding-bottom: 150%;
+  }
+
   .card__overlay {
-    opacity: 1 !important;
-    visibility: visible !important;
-    background: linear-gradient(to top, rgba(0, 114, 171, 0.7) 0%, rgba(0, 114, 171, 0) 100%);
-    padding: 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 114, 171, 0.9);
+    opacity: 0;
+    visibility: hidden;
+    transition: opacity 0.3s ease, visibility 0.3s ease;
+    display: flex;
+    align-items: flex-end;
+    border-radius: 8px;
+    padding: 20px;
+    box-sizing: border-box;
   }
-}
 
-@media (max-width: 539px) {
-  .card:nth-child(n + 3) {
-    display: none;
+  @media (min-width: 960px) {
+    .card:hover .card__overlay {
+      opacity: 1;
+      visibility: visible;
+    }
   }
-}
 
-.card__content {
-  width: 100%;
-}
+  @media (max-width: 959px) {
+    .card__overlay {
+      opacity: 1 !important;
+      visibility: visible !important;
+      background: linear-gradient(to top, rgba(0, 114, 171, 0.7) 0%, rgba(0, 114, 171, 0) 100%);
+      padding: 10px;
+    }
+  }
 
-.card__category {
-  font-size: 1.25rem;
-  margin-bottom: 5px;
-  color: #fff;
-  font-weight: 500;
-}
+  @media (max-width: 539px) {
+    .card:nth-child(n + 3) {
+      display: none;
+    }
+  }
 
-.card__heading {
-  font-size: 1rem;
-  color: rgba(255, 255, 255, 0.7);
-  font-weight: 500;
-}
+  .card__content {
+    width: 100%;
+  }
 
-.mobile-swiper {
-  width: 100%;
-}
+  .card__category {
+    font-size: 1.25rem;
+    margin-bottom: 5px;
+    color: #fff;
+    font-weight: 500;
+  }
 
-.swiper-card__background {
-  width: 100%;
-  padding-bottom: 150%;
-  background-size: cover;
-  background-position: center;
-  position: relative;
-  border-radius: 8px;
-  overflow: hidden;
-}
+  .card__heading {
+    font-size: 1rem;
+    color: rgba(255, 255, 255, 0.7);
+    font-weight: 500;
+  }
 
-.swiper-card__overlay {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: linear-gradient(
+  .mobile-swiper {
+    width: 100%;
+  }
+
+  .swiper-card__background {
+    width: 100%;
+    padding-bottom: 150%;
+    background-size: cover;
+    background-position: center;
+    position: relative;
+    border-radius: 8px;
+    overflow: hidden;
+  }
+
+  .swiper-card__overlay {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: linear-gradient(
       to top,
       rgba(0, 114, 171, 0.9) 0%,
       rgba(0, 114, 171, 0) 100%
-  );
-  padding: 15px;
-  box-sizing: border-box;
-}
+    );
+    padding: 15px;
+    box-sizing: border-box;
+  }
 
-.swiper-card__content {
-  width: 100%;
-}
+  .swiper-card__content {
+    width: 100%;
+  }
 
-.swiper-card__category {
-  font-size: 1.25rem;
-  margin-bottom: 5px;
-  color: #fff;
-  font-weight: 500;
-}
+  .swiper-card__category {
+    font-size: 1.25rem;
+    margin-bottom: 5px;
+    color: #fff;
+    font-weight: 500;
+  }
 
-.swiper-card__heading {
-  font-size: 1rem;
-  color: rgba(255, 255, 255, 0.7);
-  font-weight: 500;
-}
+  .swiper-card__heading {
+    font-size: 1rem;
+    color: rgba(255, 255, 255, 0.7);
+    font-weight: 500;
+  }
 </style>
--- src/pages/Main/Section4.vue
+++ src/pages/Main/Section4.vue
@@ -1,73 +1,73 @@
 <script>
-import SideBar from "@/pages/Main/SideBar.vue";
-import Sections from "@/components/Sections.vue";
-import More from "@/components/Buttons/more.vue";
-import CardGrid from "@/pages/Main/CardGrid.vue";
-import api from "@/axios";
-import {mapGetters} from "vuex";
+  import SideBar from '@/pages/Main/SideBar.vue';
+  import Sections from '@/components/Sections.vue';
+  import More from '@/components/Buttons/more.vue';
+  import CardGrid from '@/pages/Main/CardGrid.vue';
+  import api from '@/axios';
+  import { mapGetters } from 'vuex';
 
-export default {
-  name: "Section4",
-  components: {CardGrid, More, Sections, SideBar},
-  props: {
-    title: {
-      type: String,
-      required: true,
-      default: "{{ popular_persons }}"
+  export default {
+    name: 'Section4',
+    components: { CardGrid, More, Sections, SideBar },
+    props: {
+      title: {
+        type: String,
+        required: true,
+        default: '{{ popular_persons }}',
+      },
+      btn_title: {
+        type: String,
+        default: '{{ learn_more }}',
+      },
+      isBackground: {
+        type: Boolean,
+        default: false,
+      },
     },
-    btn_title: {
-      type: String,
-      default: "{{ learn_more }}"
+    data() {
+      return {
+        page: '04',
+        persons: [],
+      };
     },
-    isBackground: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data() {
-    return {
-      page: "04",
-      persons: [],
-    };
-  },
-  computed: {
-    ...mapGetters(['currentLanguage']),
-  },
-  watch: {
-    currentLanguage(newLang) {
+    computed: {
+      ...mapGetters(['currentLanguage']),
+    },
+    watch: {
+      currentLanguage(newLang) {
+        this.getCulture();
+      },
+    },
+    mounted() {
       this.getCulture();
     },
-  },
-  mounted() {
-    this.getCulture();
-  },
-  methods: {
-    getCulture() {
-      api.get(`/famous-persons/?lang_code=${this.currentLanguage}`)
+    methods: {
+      getCulture() {
+        api.get(`/famous-persons/?lang_code=${this.currentLanguage}`)
           .then((response) => {
             this.persons = response.data;
           })
           .catch((error) => {
-            console.error("известные личности", error);
+            console.error('известные личности', error);
           });
+      },
+      goToFamousPersons() {
+        this.$router.push('/famous-persons');
+      },
     },
-    goToFamousPersons() {
-      this.$router.push('/famous-persons')
-    }
-  },
-};
+  };
 </script>
 
 <template>
   <div class="section" id="section4">
-    <side-bar :page="page" :icon="false" :isBackground="isBackground"/>
+    <side-bar class="side" :page="page" :icon="false" :isBackground="isBackground" />
     <div class="content">
       <sections>
         <template #title>
           {{ title }}
         </template>
         <template #title-button>
-          <more @click="goToFamousPersons" :title="btn_title"/>
+          <more @click="goToFamousPersons" :title="btn_title" />
         </template>
         <template #content>
           <CardGrid :cards="persons"></CardGrid>
@@ -78,25 +78,21 @@ export default {
 </template>
 
 <style scoped>
-.section {
-  display: flex;
-}
-
-.content {
-  width: 70%;
-  display: flex;
-  justify-content: center;
-  margin-left: auto;
-  margin-right: auto;
-  align-items: center;
-}
-
-@media (max-width: 1024px) {
-  .content {
-    width: 90%;
+  .section {
+    display: flex;
+    position: relative;
   }
-}
 
-@media (max-width: 768px) {
-}
+  .content {
+    display: flex;
+    margin: 0 auto;
+  }
+
+  @media (max-width: 1024px) {
+    .content {
+    }
+  }
+
+  @media (max-width: 768px) {
+  }
 </style>
